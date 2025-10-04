27ad156cb26d1370298996cd0450059f9b18b270
ManuelGraziosi <manuel.graziosi@gmail.com>
2025-10-04 13:03:16 +0200

    feat: snack2 completed

--- snack2/js/script.js
+++ snack2/js/script.js
@@ -1,45 +1,68 @@
 const rootElemHandler = document.getElementById("root");
 
-/** *************** SNACK 1 *************** */
+/** *************** SNACK 2 *************** */
 // Main Code
-const biciclette = [
-    { nome: "Mountain Bike", peso: 14 },
-    { nome: "Bici da Corsa", peso: 7 },
-    { nome: "Bici da Città", peso: 12 },
-    { nome: "Bici Elettrica", peso: 22 },
-    { nome: "Bici Pieghevole", peso: 11 },
-    { nome: "BMX", peso: 10 },
-    { nome: "Gravel Bike", peso: 9 },
-    { nome: "Fat Bike", peso: 16 },
-    { nome: "Tandem", peso: 20 },
-    { nome: "Cruiser", peso: 13 }
+const footballTeams = [
+  { nome: "Juventus", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Inter", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Milan", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Napoli", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Roma", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Lazio", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Fiorentina", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Atalanta", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Torino", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Bologna", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Sassuolo", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Genoa", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Cagliari", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Udinese", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Empoli", puntiFatti: 0, falliSubiti: 0 }
 ];
 
-const biciclettaLeggera = findLighterBicycle(biciclette);
-console.log("biciclettaLeggera", biciclettaLeggera);
-rootElemHandler.innerHTML = `
-    <div>
-        <h2>Bici piu legera:</h2>
-        <p>Nome: ${biciclettaLeggera.nome}</p>
-        <p>Peso: ${biciclettaLeggera.peso}</p>
-    </div>
-`
+console.log(footballTeams);
+applyRandomValue(footballTeams);
+console.log(footballTeams);
+
+const compressedTeams = [];
+for (i = 0; i < footballTeams.length; i++) {
+  const curTeam = footballTeams[i];
+  // console.log("curTeam", curTeam);
+
+  const newDataTeam = {};
+  for (let key in curTeam) {
+    if (key !== "puntiFatti") {
+      newDataTeam[key] = curTeam[key];
+    }
+  }
+  // console.log("newDataTeam", newDataTeam);
+  compressedTeams.push(newDataTeam);
+}
+console.log("compressedTeams", compressedTeams);
+
 // Function Definition
 /**
- * Description: Function that finds the element of the array with the lowest weight. Returns the element
- * @param {Array} arrayBicycle
- * @returns {Object}
+ * Description: Void Function that apply random value to the array element. 
+ * @param {Array} arrayTeam
+ * 
  */
 
-function findLighterBicycle(arrayBicycle) {
-    let lighterBicycle = arrayBicycle[0];
-    for (let i = 1; i < arrayBicycle.length; i++) {
-        const curBicycle = arrayBicycle[i];
-        if (curBicycle.peso < lighterBicycle.peso) {
-            lighterBicycle = curBicycle;
-        }
-    }
-    return lighterBicycle
+function applyRandomValue(arrayTeam) {
+  arrayTeam.forEach(element => {
+    element.puntiFatti = randomNumber(0, 100);
+    element.falliSubiti = randomNumber(2, 50);
+  });
 }
 
-/** *************** /SNACK 1 *************** */
+/**
+ * Description: function that generate random value between a range min,max
+ * 
+ * @param {number} min 
+ * @param {number} max 
+ * @returns 
+ */
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+/** *************** /SNACK 2 *************** */
