e69264c4dc62280435697df6435ea405f2a8edf3
ManuelGraziosi <manuel.graziosi@gmail.com>
2025-10-06 13:02:26 +0200

    feat: snack4 added more function version

--- snack4/js/script.js
+++ snack4/js/script.js
@@ -2,6 +2,7 @@ const rootElemHandler = document.getElementById("root");
 
 const array1 = [1, 2, 3, 4, 5];
 const array2 = ["a", "b", "c"];
+// const array2 = ["a", "b", "c", "d", "e", "f", "g", "h"];
 /** *************** SNACK 4 *************** */
 // Main Code
 console.log("Snack4");
@@ -11,6 +12,11 @@ console.log("mergeTestV1", mergeTestV1);
 const mergeTestV2 = mergeTwoArrayV2(array1, array2);
 console.log("mergeTestV2", mergeTestV2);
 
+const mergeTestV3A = mergeTwoArrayV3(array1, array2);
+console.log("mergeTestV3A", mergeTestV3A);
+
+const mergeTestV3B = mergeTwoArrayV3(array2, array1);
+console.log("mergeTestV3B", mergeTestV3B);
 
 // Function Definition
 /**
@@ -56,6 +62,37 @@ function mergeTwoArrayV2(inputArray1, inputArray2) {
     } else if (inputArray1.length > inputArray2.length) {
         const residualArray = inputArray1.slice(inputArray2.length);
         resultArray.push(...residualArray);
+    }
+    return resultArray;
+}
+
+// Function Definition
+/**
+ * Descrition: funcion return a new array that marge two array reading alternatively value from the array
+ * aka V2 but without array's method
+ * @param {Array} inputArray1 
+ * @param {Array} inputArray2 
+ * @returns {Array}
+ */
+function mergeTwoArrayV3(inputArray1, inputArray2) {
+    const resultArray = [];
+    for (let i = 0; i < Math.min(inputArray1.length, inputArray2.length); i++) {
+        const curElemArray1 = inputArray1[i];
+        const curElemArray2 = inputArray2[i];
+        resultArray.push(curElemArray1)
+        resultArray.push(curElemArray2)
+    }
+
+    if (inputArray1.length < inputArray2.length) {
+        for (let i = inputArray1.length; i < inputArray2.length; i++) {
+            const residualArrayCurElem = inputArray2[i];
+            resultArray.push(residualArrayCurElem);
+        }
+    } else if (inputArray1.length > inputArray2.length) {
+        for (let i = inputArray2.length; i < inputArray1.length; i++) {
+            const residualArrayCurElem = inputArray1[i];
+            resultArray.push(residualArrayCurElem);
+        }
     }
     return resultArray;
 }
